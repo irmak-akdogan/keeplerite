1a2dca577702dad7df9471731416510806758825
Irmak97 <irmakakdogan04@gmail.com>
2025-04-02 14:54:46 -0400

    added imports

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
 """exampy: an example Python package"""
 __version__= "0.2"
 
-from .targetpixelfile import *
-from .lightcurve import *
-from .periodogram import *
+from .astronomyobject import *
--- astronomyobject/__init__.py
+++ astronomyobject/__init__.py
@@ -2,3 +2,7 @@ __doc__= """exampy.integrate: submodule with utilities for calculating
 the integral of functions"""
 
 from ._astronomyobject import *
+
+from .targetpixelfile import *
+from .lightcurve import *
+from .periodogram import *
